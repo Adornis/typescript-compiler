0981fee6c1ed83f4bb66f469f14414d39f875795
Yorrd <yorrd@users.noreply.github.com>
2019-02-03 16:24:41 +0100

    fix missing dependencies

--- package.js
+++ package.js
@@ -1,43 +1,62 @@
 Package.describe({
-  name: 'adornis:typescript-compiler',
-  version: '0.12.5',
-  summary: 'TypeScript Compiler for Meteor, based on barbatus:typescript-compiler',
-  git: 'https://github.com/adornis/typescript-compiler',
-  documentation: 'README.md',
+  name: "adornis:typescript-compiler",
+  version: "0.12.5",
+  summary:
+    "TypeScript Compiler for Meteor, based on barbatus:typescript-compiler",
+  git: "https://github.com/adornis/typescript-compiler",
+  documentation: "README.md"
 });
 
 Npm.depends({
   // 'meteor-typescript': '0.9.0', // is now in this directory because I'm lazy
-  async: '2.5.0',
-  colors: '1.1.2',
-  chalk: '2.4.1',
-  'random-js': '1.0.8',
-  'object-sizeof': '1.3.0',
-  underscore: '1.9.1',
+  async: "2.5.0",
+  colors: "1.1.2",
+  chalk: "2.4.1",
+  "random-js": "1.0.8",
+  "object-sizeof": "1.3.0",
+  underscore: "1.9.1",
+  diff: "2.2.2",
+  "lru-cache": "4.1.1",
+  typescript: "2.8.3"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.1');
+  api.versionsFrom("1.4.1");
 
-  api.use(['ecmascript@0.10.8', 'check@1.0.5', 'underscore@1.0.4'], 'server');
+  api.use(["ecmascript@0.10.8", "check@1.0.5", "underscore@1.0.4"], "server");
 
-  api.addFiles(['logger.js', 'file-utils.js', 'typescript-compiler.js', 'typescript.js', 'utils.js'], 'server');
+  api.addFiles(
+    [
+      "logger.js",
+      "file-utils.js",
+      "typescript-compiler.js",
+      "typescript.js",
+      "utils.js"
+    ],
+    "server"
+  );
 
-  api.export(['TypeScript', 'TypeScriptCompiler'], 'server');
+  api.export(["TypeScript", "TypeScriptCompiler"], "server");
 });
 
 Package.onTest(function(api) {
   api.use([
-    'tinytest',
-    'ecmascript',
-    'underscore',
-    'practicalmeteor:sinon',
-    'practicalmeteor:chai',
-    'practicalmeteor:mocha',
-    'meteortesting:mocha',
-    'dispatch:mocha-phantomjs',
+    "tinytest",
+    "ecmascript",
+    "underscore",
+    "practicalmeteor:sinon",
+    "practicalmeteor:chai",
+    "practicalmeteor:mocha",
+    "meteortesting:mocha",
+    "dispatch:mocha-phantomjs"
   ]);
-  api.use('adornis:typescript-compiler');
+  api.use("adornis:typescript-compiler");
 
-  api.addFiles(['tests/server/unit/input-file.js', 'tests/server/unit/compiler-tests_spec.js'], 'server');
+  api.addFiles(
+    [
+      "tests/server/unit/input-file.js",
+      "tests/server/unit/compiler-tests_spec.js"
+    ],
+    "server"
+  );
 });
