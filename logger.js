5f385a024ff11f577ec3ef9515d82bcb6fc02ea2
Elias Künstler <elias@adornis.de>
2019-02-16 13:05:24 +0100

    added env var to disable warnings

--- logger.js
+++ logger.js
@@ -1,9 +1,10 @@
-const util = Npm.require('util');
-const chalk = Npm.require('chalk');
+const util = Npm.require("util");
+const chalk = Npm.require("chalk");
 
 class Logger_ {
   constructor() {
     this.llevel = process.env.TYPESCRIPT_LOG;
+    this.disableWarnings = process.env.TYPESCRIPT_DISABLE_WARNINGS;
   }
 
   newProfiler(name) {
@@ -31,15 +32,15 @@ class Logger_ {
   }
 
   warn(msg, ...other) {
-    process.stdout.write(chalk.bold.yellow(msg) + chalk.reset(other.join(' ')) + '\n');
+    if (!this.disableWarnings) process.stdout.write(chalk.bold.yellow(msg) + chalk.reset(other.join(" ")) + "\n");
   }
 
   error(msg, ...other) {
-    process.stdout.write(chalk.bold.red(msg) + chalk.reset(other.join(' ')) + '\n');
+    process.stdout.write(chalk.bold.red(msg) + chalk.reset(other.join(" ")) + "\n");
   }
 
   info(msg) {
-    process.stdout.write(chalk.bold.green(msg) + chalk.dim(' ') + '\n');
+    process.stdout.write(chalk.bold.green(msg) + chalk.dim(" ") + "\n");
   }
 
   debug(msg, ...args) {
@@ -63,14 +64,14 @@ class Profiler {
   }
 
   start() {
-    console.log('%s started', this.name);
-    console.time(util.format('%s time', this.name));
+    console.log("%s started", this.name);
+    console.time(util.format("%s time", this.name));
     this._started = true;
   }
 
   end() {
     if (this._started) {
-      console.timeEnd(util.format('%s time', this.name));
+      console.timeEnd(util.format("%s time", this.name));
     }
   }
 }
