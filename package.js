709ad38e4292d502a99b16a61ab194874b24205d
Yorrd <yorrd.adornis@gmail.com>
2019-04-20 14:43:32 +0200

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'adornis:typescript-compiler',
-  version: '0.12.11',
+  version: '0.12.12',
   summary: 'TypeScript Compiler for Meteor, based on barbatus:typescript-compiler',
   git: 'https://github.com/adornis/typescript-compiler',
   documentation: 'README.md',
